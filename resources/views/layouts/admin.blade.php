677ff2d7691023bca25667bfafeca67bb5eeac80
devaaa <renaldeva107@gmail.com>
2026-06-12 04:25:34 +0700

    PWA IPHONE

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,8 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="theme-color" content="#0ea5e9">
+    <<meta name="theme-color" content="#0ea5e9">
+
     <link rel="manifest" href="/manifest.json">
+    
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="default">
+    <meta name="apple-mobile-web-app-title" content="AquaSync">
+    
     <link rel="apple-touch-icon" href="/icons/icon-192.png">
     <title>@yield('title','Dashboard') – AquaSync Admin</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
