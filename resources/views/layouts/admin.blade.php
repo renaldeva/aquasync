fee4bff6e6cd8c500059aebfde8443deb44a7019
devaaa <renaldeva107@gmail.com>
2026-06-12 05:57:46 +0700

    vitejs

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -254,6 +254,7 @@
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+@vite(['resources/js/app.js'])
 <script>
 lucide.createIcons();
 setTimeout(()=>{const t=document.getElementById('tw');if(t){t.style.opacity='0';t.style.transition='opacity .4s';setTimeout(()=>t.remove(),400)}},4000);
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -671,6 +671,7 @@
 
 {{-- Scripts --}}
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+@vite(['resources/js/app.js'])
 <script>
     // Init Lucide icons
     lucide.createIcons();
